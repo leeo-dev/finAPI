19a6588187019b76ba1a8da86ff085787b02c456
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-12-12 16:26:04 -0300

    Task: Route withdraw

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const res = require("express/lib/response");
 const { v4: uuidV4 } = require("uuid");
 const app = express();
 const PORT = 3000;
-
+//https://github.com/viictorcamposs/finAPI/blob/main/src/index.js
 app.use(express.json());
-const customers = [];
+const customers = [
+  {
+    id: "a371f25d-f5e9-45dd-a28b-62a0dba31f09",
+    name: "Leonardo Albuquerque",
+    cpf: "05095621169",
+    statement: [
+      {
+        description: "Ganhos",
+        amount: 5000,
+        createdAt: "2021-12-12T18:41:09.104Z",
+        type: "credit",
+      },
+      {
+        description: "Jobs",
+        amount: 3000,
+        createdAt: "2021-12-12T18:41:18.435Z",
+        type: "credit",
+      },
+    ],
+  },
+];
 //Middleware
 function verifyExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
@@ -16,6 +35,15 @@ function verifyExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statementArray) {
+  const amount = statementArray.map((statement) => statement.amount);
+  //prettier-ignore
+  const balance = amount.reduce((prevValue, nextValue) => prevValue + nextValue,0);
+  console.log(balance);
+  return balance;
+}
+
+// app.use(verifyExistsAccountCPF);
 app.post("/account", (request, response) => {
   const { name, cpf } = request.body;
   const customerAlreadyExists = customers.some(
@@ -27,11 +55,40 @@ app.post("/account", (request, response) => {
   customers.push(customer);
   return response.status(201).json(customer);
 });
-// app.use(verifyExistsAccountCPF);
 app.get("/statement/", verifyExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.json(customer.statement);
 });
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit",
+  };
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+  //prettier-ignore
+  if(balance < amount) return response.status(400).json({error:"Insufficient funds."})
+  const statementOperation = {
+    type: "debit",
+    amount: -amount,
+    createdAt: new Date(),
+  };
+  customer.statement.push(statementOperation);
+  response.status(201).json({
+    statementOperation,
+    success: "Successful withdrawal.",
+  });
+});
+app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {});
 
 //prettier-ignore
 app.listen(PORT || 5000, () => console.log(`Server is running on port ${PORT}`));
